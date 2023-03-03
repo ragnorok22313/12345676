0e93be1af3ec89af9b3571ae7a28cb5e4804a078
ragnorok22313 <liu88777@126.com>
2023-03-03 19:20:21 +0800

    first.c文件首次提交

---
+++ first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	printf("test output msg 01\n");
+	printf("test output msg 01\n");
+	printf("test output msg 01\n");
+	printf("test output msg 01\n");
+	return 0;
+}
